a5b3e082f052132d7fa674f2a18a77378f75635b
Adnan <adnan@cyngn.com>
2014-08-03 17:00:20 -0700

    Keyguard: Add option to scramble pin layout when unlocking (1/2).
    Adapted to Lineage SDK and squashed with the following changes:
    
      From: Adnan Begovic <adnan@cyngn.com>
      Date: Wed, 05 Aug 2015 10:40:41 -0700
    
          Keyguard: Don't disable visibility when scrambling pin.
    
            Otherwise the first numbers text is flipped to invisible
            even though it shouldn't be.
    
          Change-Id: Ia2b80d594e7f11af7a60a85bdb4ea9909d0ac20b
    
      From: Adnan Begovic <adnan@cyngn.com>
      Date: Wed, 05 Aug 2015 16:21:27 -0700
    
          Keyguard: Fix scramblepin logic.
    
          Change-Id: I71e6d26e853fa7bfaeea4b3256902881ac7f74fc
    
      From: LuK1337 <priv.luk@gmail.com>
      Date: Sat Apr 24 18:14:46 2021 +0200
    
          Keyguard: Fix mapping mismatch between KeyEvent and NumPadKey
    
          Change-Id: Ida28a4f037c2f3fa843ac136779e026020d711db
    
    Original-Change-Id: Ibe8611eb0b8a5bd9275bc96682d2cbafaa37d8f5
    Change-Id: I6716b1c76b69d51bd1457ce07de2274541fca1e8
    
    -----
    
    fixup! Keyguard: Add option to scramble pin layout when unlocking (2/2).
    
    Fix issue introduced with 13 QPR2 in which the PIN layout is not
    randomized every time the PIN entry view appears.
    
    Issue: calyxos#1557
    Change-Id: I44247941a02c95cc2ec53fc42ddb1337b61f692e
    Co-authored-by: Tommy Webb <tommy@calyxinstitute.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5574,6 +5574,13 @@ public final class Settings {
         public static final String LOCKSCREEN_MEDIA_BLUR = "lockscreen_media_blur";
 
         /**
+         * Whether to scramble a pin unlock layout
+         * 0 = 0ff, 1 = on
+         * @hide
+         */
+        public static final String LOCKSCREEN_PIN_SCRAMBLE_LAYOUT = "lockscreen_scramble_pin_layout";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
