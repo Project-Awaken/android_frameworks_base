c455b3452d569a046637d36d5ebbf67026dddf8e
Evisceration <eviscerationls@gmail.com>
2015-01-01 14:29:50 +0000

    Quick settings: Add Haptic Feedback to tiles [1/2]
    mydongistiny: updated for oreo
    neobuddy89: rewrite using TunerAPI
    saikiran2001: updated for android 11
    
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9165,6 +9165,12 @@ public final class Settings {
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
         /**
+         * Whether to vibrate when quick settings tile is pressed.
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_VIBRATE = "quick_settings_vibrate";
+
+        /**
          * Wheter to have transparent background on lockscreen notifications
          *
          * @hide
