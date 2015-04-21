bcc2b94daf2eaee48419408dc74fe19c9d970d87
Michael Bestas <mikeioannina@gmail.com>
2015-04-22 02:15:28 +0300

    SystemUI: Add heads up tile
    Change-Id: I0e3cff73752c59d804dcec43b86524c2e8df4f57

--- packages/SystemUI/src/com/android/systemui/awaken/AwakenModule.kt
+++ packages/SystemUI/src/com/android/systemui/awaken/AwakenModule.kt
@@ -19,6 +19,7 @@ package com.android.systemui.awaken
 
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.CaffeineTile
+import com.android.systemui.qs.tiles.HeadsUpTile
 
 import dagger.Binds
 import dagger.Module
@@ -32,4 +33,10 @@ interface AwakenModule {
     @IntoMap
     @StringKey(CaffeineTile.TILE_SPEC)
     fun bindCaffeineTile(caffeineTile: CaffeineTile): QSTileImpl<*>
+
+    /** Inject HeadsUpTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(HeadsUpTile.TILE_SPEC)
+    fun bindHeadsUpTile(headsUpTile: HeadsUpTile): QSTileImpl<*>
 }
