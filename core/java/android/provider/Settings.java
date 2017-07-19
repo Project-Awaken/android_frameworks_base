643a1d07ab74759fbd0e91e7c6f91e9589e42488
Pranav Vashi <neobuddy89@gmail.com>
2017-07-19 23:46:45 +0530

    base: Allow using 4G icon instead of LTE [1/2]
    Change-Id: Ieb912c64a60ddd42fe7b21d18dadea293ff159c5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5360,6 +5360,12 @@ public final class Settings {
         public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
 
         /**
+         * Whether to display 4G icon instead LTE
+         * @hide
+         */
+        public static final String SHOW_FOURG_ICON = "show_fourg_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
