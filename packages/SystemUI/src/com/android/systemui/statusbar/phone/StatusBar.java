a7817df6d33ffc7f6245c278398405c069665f40
Decad3nce <decad3nce@gmail.com>
2017-09-28 21:16:26 +0800

    SystemUI: HeadsUp blacklists [1/2]
    @xyyx: reworked
    
    Change-Id: I4aa328464406927a9ead708a1efbe37f9ab5d689
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3898,6 +3898,12 @@ public class StatusBar extends SystemUI implements
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_MEDIA_BLUR),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.HEADS_UP_STOPLIST_VALUES),
+                    false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.HEADS_UP_BLACKLIST_VALUES),
+                    false, this);
         }
 
         @Override
@@ -3912,11 +3918,14 @@ public class StatusBar extends SystemUI implements
                     Settings.System.LOCKSCREEN_MEDIA_BLUR))) {
                 setLockScreenMediaBlurLevel();
             }
+            update();
         }
 
         public void update() {
             setDoubleTapToSleepGesture();
             setLockScreenMediaBlurLevel();
+            setHeadsUpStoplist();
+            setHeadsUpBlacklist();
         }
     }
 
@@ -3930,6 +3939,16 @@ public class StatusBar extends SystemUI implements
         if (mMediaManager != null) {
             mMediaManager.setLockScreenMediaBlurLevel();
         }
+    }
+
+    private void setHeadsUpStoplist() {
+        if (mPresenter != null)
+            mPresenter.setHeadsUpStoplist();
+    }
+
+    private void setHeadsUpBlacklist() {
+        if (mPresenter != null)
+            mPresenter.setHeadsUpBlacklist();
     }
 
     private final BroadcastReceiver mBannerActionBroadcastReceiver = new BroadcastReceiver() {
