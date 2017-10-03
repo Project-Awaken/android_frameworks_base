f18b4c0790a185b77191dc252b361539d88a7bac
cphelps76 <cphelps76@droidconcepts.com>
2017-10-03 20:02:52 +0200

    BatteryBar [1/2]
    {x} Adds Battery bar with following features:
    {x} location set
    {x} color
    {x} aligment
    {x} thickness
    {x} charging animation
    (1/2)
    
    PureNexus Changes: fix and add support for 5.1
    
    AICP edit:
    Don't add navigation bar as location for now
    
    Change-Id: Ie24df2c65a7458dcfe8aebc769d774977d8bd840
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5879,6 +5879,32 @@ public final class Settings {
         public static final String SENSOR_BLOCKED_APP_DUMMY = "sensor_blocked_app_dummy";
 
         /**
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
