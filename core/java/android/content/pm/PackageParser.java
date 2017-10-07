6004328d01cf7bd759d878e17c849c0914978f8b
Daniel Micay <danielmicay@gmail.com>
2017-10-07 15:54:42 -0400

    add special runtime permission for other sensors
    This covers sensors not included in the existing runtime permission for
    body sensors.
    
    Ported from 10: 9ec9f7f521323552fa658b46862c8408f1a7b41b
    
    Changes from 10:
    - Needed to run `m api-stubs-docs-non-updatable-update-current-api`
    to fix the "You have tried to change the API from what has been
    previously approved" errors.
    
    Change-Id: I5ba540230262f69bf0b1b0af5e82b0e78a64005a

--- core/java/android/content/pm/PackageParser.java
+++ core/java/android/content/pm/PackageParser.java
@@ -294,6 +294,8 @@ public class PackageParser {
                     android.os.Build.VERSION_CODES.CUR_DEVELOPMENT + 1, 0, GmsInfo.PACKAGE_PLAY_STORE),
             new PackageParser.NewPermissionInfo(android.Manifest.permission.UPDATE_PACKAGES_WITHOUT_USER_ACTION,
                     android.os.Build.VERSION_CODES.CUR_DEVELOPMENT + 1, 0, GmsInfo.PACKAGE_PLAY_STORE),
+            new PackageParser.NewPermissionInfo(android.Manifest.permission.OTHER_SENSORS,
+                    android.os.Build.VERSION_CODES.CUR_DEVELOPMENT + 1, 0),
             new PackageParser.NewPermissionInfo(android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
                     android.os.Build.VERSION_CODES.DONUT, 0),
             new PackageParser.NewPermissionInfo(android.Manifest.permission.READ_PHONE_STATE,
