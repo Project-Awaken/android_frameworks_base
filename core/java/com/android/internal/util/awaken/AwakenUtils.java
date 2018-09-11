0b73eefb73321f53fff40d12b39c913931cddbfe
ezio84 <brabus84@gmail.com>
2018-09-11 04:08:01 -0500

    base: AwakenUtils: Add method to turn off the screen
    Change-Id: Ie65d8ebe354996ae8d67ac950d635a1169830931

--- core/java/com/android/internal/util/awaken/AwakenUtils.java
+++ core/java/com/android/internal/util/awaken/AwakenUtils.java
@@ -21,7 +21,9 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
+import android.os.PowerManager;
 import android.os.RemoteException;
+import android.os.SystemClock;
 import android.view.IWindowManager;
 import android.view.WindowManagerGlobal;
 
@@ -44,6 +46,14 @@ public class AwakenUtils {
             wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
         } catch (RemoteException e) {
             e.printStackTrace();
+        }
+    }
+
+    // Method to turn off the screen
+    public static void switchScreenOff(Context ctx) {
+        PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
+        if (pm!= null) {
+            pm.goToSleep(SystemClock.uptimeMillis());
         }
     }
 }
