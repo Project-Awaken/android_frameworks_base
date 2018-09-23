0387d082c618d057356d3577524545e87c4e743b
ezio84 <brabus84@gmail.com>
2018-09-24 00:21:01 +0530

    base: Option for Up, Down as well for Network Traffic [1/2]
    [spezi77]
    * Adapt for OOS style network traffic indicator
    * For now skipped logic to show arrow indicators (follows in a separate commit)
    
    Co-authored-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5796,6 +5796,12 @@ public final class Settings {
         public static final String POCKET_SERVICE = "pocket";
 
         /**
+         * What to show in network traffic indicator in statusbar
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_TYPE = "network_traffic_type";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
