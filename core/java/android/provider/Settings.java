29cdb12a5da4364cb50d1558c193bbe8c3fda024
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2019-01-27 16:06:14 +0000

    Ambient brightness values: use a custom system setting [1/2]
    Change-Id: I621125a2669d9c2180341704b046d2089a996b86
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4978,6 +4978,13 @@ public final class Settings {
         public static final String CUSTOM_AMBIENT_POCKETMODE_GESTURE = "custom_ambient_pocketmode_gesture";
 
         /**
+         * Ambient screen settings
+         * "screen_brightness_array=0:1:2:3:4"
+         * @hide
+         */
+        public static final String ALWAYS_ON_DISPLAY_CONSTANTS_CUST = "always_on_display_constants_cust";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
