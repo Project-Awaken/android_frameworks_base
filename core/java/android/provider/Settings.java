0cda5ce1b938197221f7a2ffd8151bbb4be29b7c
maxwen <max.weninger@gmail.com>
2019-11-27 16:27:40 +0100

    SystemUI: bring back old style mobile data indicators
    TODO maybe add settings observer to do update faster then
    waiting for the next update
    
    Change-Id: I3367bf0b7ff5b6483efc820a86dec8469af6e034
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5378,6 +5378,11 @@ public final class Settings {
         };
 
         /**
+         * @hide
+         */
+        public static final String USE_OLD_MOBILETYPE = "use_old_mobiletype";
+
+        /**
          * Three Finger Gesture from Oppo
          * @hide
          */
