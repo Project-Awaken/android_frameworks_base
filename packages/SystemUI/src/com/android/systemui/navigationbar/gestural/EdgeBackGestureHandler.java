9610e923d51846560a9b9df638dddef637613f31
ezio84 <brabus84@gmail.com>
2019-12-15 10:38:37 +0100

    base: Allow to set gestures back swipe deadzone [1/2]
    So it's easier to open apps left drawer or avoid unwanted back swipes
    when using gallery apps.
    
    Change-Id: I805941e7b9de44f3ffdb115ccaad9fa8c0712576
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -210,6 +210,8 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
     private int mEdgeWidthRight;
     // The bottom gesture area height
     private float mBottomGestureHeight;
+    // Displaysize divider to check the edge height where touch down is allowed
+    private int mYDeadzoneDivider = 0;
     // The slop to distinguish between horizontal and vertical motion
     private float mTouchSlop;
     // The threshold for triggering back
@@ -412,6 +414,8 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
         mEdgeWidthRight = mGestureNavigationSettingsObserver.getRightSensitivity(res);
         mIsBackGestureAllowed =
                 !mGestureNavigationSettingsObserver.areNavigationButtonForcedVisible();
+
+        mYDeadzoneDivider = mGestureNavigationSettingsObserver.getDeadZoneMode();
 
         final DisplayMetrics dm = res.getDisplayMetrics();
         final float defaultGestureHeight = res.getDimension(
@@ -761,6 +765,10 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
         // gesture
         final boolean isInsidePip = mIsInPip && mPipExcludedBounds.contains(x, y);
         if (isInsidePip || mNavBarOverlayExcludedBounds.contains(x, y)) {
+            return false;
+        }
+
+        if (mYDeadzoneDivider != 0 && y < (mDisplaySize.y / mYDeadzoneDivider)) {
             return false;
         }
 
