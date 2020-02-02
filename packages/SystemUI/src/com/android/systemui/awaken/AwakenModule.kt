118f41ee63fb78b0aeca4cb869a4da841ff65018
ezio84 <brabus84@gmail.com>
2020-02-02 17:16:40 +0100

    SystemUI: Add Mono audio tile (aka headphone in one ear only)
    thanks buffal0b1ll for the suggestion!
    
    Change-Id: I284e6c3a1418dd88fa8457e59dffeabbfed93ceb
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    -----
    
    Mono/Stereo QS tile: Add long click intent
    
    Change-Id: I6fa456631b4513f4009e843d27bb7d2368d0506b
    
    -----
    
    MonoToggleTile: Use secondary label to indicate mode
    
    * Make default state inactive while we're at it
    
    Change-Id: I1aa1ec545743dd42c46996fa94bfc1c895a05a7c
    Co-authored-by: Alex <du.alexcruz@gmail.com>
    Co-authored-by: PainKiller3 <ninadpatil100@gmail.com>

--- packages/SystemUI/src/com/android/systemui/awaken/AwakenModule.kt
+++ packages/SystemUI/src/com/android/systemui/awaken/AwakenModule.kt
@@ -26,6 +26,7 @@ import com.android.systemui.qs.tiles.CompassTile
 import com.android.systemui.qs.tiles.CPUInfoTile
 import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.HeadsUpTile
+import com.android.systemui.qs.tiles.MonoToggleTile
 import com.android.systemui.qs.tiles.SoundTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
@@ -86,6 +87,12 @@ interface AwakenModule {
     @IntoMap
     @StringKey(HeadsUpTile.TILE_SPEC)
     fun bindHeadsUpTile(headsUpTile: HeadsUpTile): QSTileImpl<*>
+
+    /** Inject MonoToggleTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(MonoToggleTile.TILE_SPEC)
+    fun bindMonoToggleTile(monoToggleTile: MonoToggleTile): QSTileImpl<*>
 
     /** Inject SoundTile into tileMap in QSModule */
     @Binds
