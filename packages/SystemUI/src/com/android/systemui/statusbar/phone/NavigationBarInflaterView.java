7723a357848a762901aebdc8dea0aa7d343a8514
Alex <du.alexcruz@gmail.com>
2020-02-28 04:12:52 -0500

    Navbar: Ensure 2/3 button layout options play nice with gesture navbar
    The 2 and 3 button navbar layout options were causing the gesture 'pill' to
    disappear when enabled. This commit ensures that the pill survives when switching
    and keeps the user's layout for 2 and 3 button navbar in play when they return.
    
    Test:
    - Enable 2 or 3 button navbar
    - Go into layout options
    - Set the layout to compact or any other option but 'Normal'
    - Enable the gesture navbar
    
    Change-Id: Id4f0bc8c7c155090bb9f7ce24edd5bdbbd7553d5
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarInflaterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarInflaterView.java
@@ -175,6 +175,9 @@ public class NavigationBarInflaterView extends FrameLayout
             mInverseLayout = TunerService.parseIntegerSwitch(newValue, true);
             updateLayoutInversion();
         }
+        if (QuickStepContract.isGesturalMode(mNavBarMode)) {
+            setNavigationBarLayout(newValue);
+        }
     }
 
     public void setNavigationBarLayout(String layoutValue) {
