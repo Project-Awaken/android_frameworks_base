c2ad3407f62402c8491da788a659b6368301ccee
JBCDroid2 <jonathan.lyndo@outlook.com>
2020-06-17 20:49:02 +0530

    Switch Styles : Add telegram switch style [1/3]
    Change-Id: I6da058113ea4d954858a9ed84f6b06ac8494a497
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- core/java/com/android/internal/util/awaken/ThemesUtils.java
+++ core/java/com/android/internal/util/awaken/ThemesUtils.java
@@ -87,6 +87,7 @@ public class ThemesUtils {
         "com.android.system.switch.oneplus", // 1
 	"com.android.system.switch.narrow", // 2
         "com.android.system.switch.contained", // 3
+        "com.android.system.switch.telegram", // 4
         "com.android.system.switch.md2", // 5
         "com.android.system.switch.retro", // 6
     };
