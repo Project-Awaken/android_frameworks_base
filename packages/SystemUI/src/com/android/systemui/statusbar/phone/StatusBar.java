48971b7e93a51ed7407840bf5110f707d1849b62
Saikiran <bjsaikiran@gmail.com>
2020-07-11 08:33:27 +0530

    base: Remove keyguard delay completely
     * Improves fingerprint and faceunlock speed considerably
    
    Change-Id: I27b8623813564a7e82091f021ce1099660333f8a
    Signed-off-by: Saikiran <bjsaikiran@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -325,8 +325,8 @@ public class StatusBar extends SystemUI implements DemoMode,
     private static final int BRIGHTNESS_CONTROL_LONG_PRESS_TIMEOUT = 750; // ms
     private static final int BRIGHTNESS_CONTROL_LINGER_THRESHOLD = 20;
 
-    public static final int FADE_KEYGUARD_START_DELAY = 100;
-    public static final int FADE_KEYGUARD_DURATION = 300;
+    public static final int FADE_KEYGUARD_START_DELAY = 10;
+    public static final int FADE_KEYGUARD_DURATION = 10;
     public static final int FADE_KEYGUARD_DURATION_PULSING = 96;
 
     /** If true, the system is in the half-boot-to-decryption-screen state.
