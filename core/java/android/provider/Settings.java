42e8502d361c590004d2ce37b22812a8c8a3113d
cjybyjk <cjybyjk@zjnu.edu.cn>
2020-09-07 01:44:05 +0800

    base: make app volume rows optional [1/2]
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Change-Id: I5c02019a7cace127da41d72b386ad6f512871600
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5964,6 +5964,12 @@ public final class Settings {
                 "statusbar_battery_bar_blend_dark_color_reverse";
 
         /**
+         * Show app volume rows in volume panel
+         * @hide
+         */
+        public static final String SHOW_APP_VOLUME = "show_app_volume";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
