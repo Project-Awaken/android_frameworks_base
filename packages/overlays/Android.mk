45bb70d63b6b7efae0a75242a87fcd7ee75c01ed
saikiran2001 <bjsaikiran@gmail.com>
2020-10-02 08:39:28 +0200

    overlays: Build heart icon shape overlay
    There is no reason not to build this.

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -45,6 +45,7 @@ LOCAL_REQUIRED_MODULES := \
 	IconPackRoundedSettingsOverlay \
 	IconPackRoundedSystemUIOverlay \
 	IconPackRoundedThemePickerOverlay \
+        IconShapeHeartOverlay \
 	IconShapePebbleOverlay \
 	IconShapeRoundedRectOverlay \
 	IconShapeSquircleOverlay \
