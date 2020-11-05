f9072dbdc0c88e50b129e73e8f33890df4e7e902
Danny Baumann <dannybaumann@web.de>
2020-11-05 19:13:14 +0000

    base: Add back increasing ring feature (1/3)
    Change-Id: I2cd34e08badb343fa418ed07e6733e3fbe7d7d5d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6077,6 +6077,24 @@ public final class Settings {
                 "screen_flash_notification_color_global";
 
         /**
+         * Whether the phone ringtone should be played in an increasing manner
+         * @hide
+         */
+        public static final String INCREASING_RING = "increasing_ring";
+
+        /**
+         * Start volume fraction for increasing ring volume
+         * @hide
+         */
+        public static final String INCREASING_RING_START_VOLUME = "increasing_ring_start_vol";
+
+        /**
+         * Ramp up time (seconds) for increasing ring
+         * @hide
+         */
+        public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
