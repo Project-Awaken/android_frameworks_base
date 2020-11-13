d6bbc360b400eab44bd64ce630ff1b37a6a83b0d
Demon000 <demonsingur@gmail.com>
2020-11-13 21:20:54 +0100

    SystemUI: implement better partial screenshot
    commit ecd79bcd1362bf81b0ce0dfc970c3a7cef7a459e
    Author: Demon000 <demonsingur@gmail.com>
    Date:   Mon Jan 20 03:37:29 2020 +0000
    
        ScreenshotHelper: ignore timeout when taking partial screenshots
    
        User should have as much time to select the region as he wants.
    
        Change-Id: Ib9b79d5127a59d3e2af278dbc8cd054221c37ca6
    
    commit 39b2b0fa8979b2904d7435215cd13241a9ca296e
    Author: ezio84 <brabus84@gmail.com>
    Date:   Sun Dec 1 18:19:48 2019 +0100
    
        SystemUI: block gestural navigation when using partial screenshot
    
        Change-Id: I68d25a6eab9ea16707e8736b9e56055532bcdf87
    
    commit 77d3a89ebde978c195b4623fa3fbc52b75975f32
    Author: Demon000 <demonsingur@gmail.com>
    Date:   Sat Feb 1 18:24:34 2020 +0000
    
        GlobalScreenshot: do not capture multiple screenshots simultaneously
    
        Change-Id: I4c0c73759b8b0483f0d28b4c6c39e9a616c26b57
    
    Change-Id: Ib818b24bb788a19ac210378611402c96fa55bd37
    
    fixup! SystemUI: implement better partial screenshot
    
    * On R sys ui flags no longer disable edge swipe...
    
    Change-Id: I767997a82ee0fc819bbaa7bc906e89127e53a17c
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -133,6 +133,7 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
     private static final int MSG_TOGGLE_SETTINGS_PANEL             = 61 << MSG_SHIFT;
     private static final int MSG_SCREEN_PINNING_STATE_CHANGED      = 62 << MSG_SHIFT;
     private static final int MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED   = 63 << MSG_SHIFT;
+    private static final int MSG_SET_BLOCKED_GESTURAL_NAVIGATION   = 64 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -278,6 +279,7 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
         default void hideAuthenticationDialog() { }
         default void showInDisplayFingerprintView() { }
         default void hideInDisplayFingerprintView() { }
+        default void setBlockedGesturalNavigation(boolean blocked) {}
 
         /**
          * @see IStatusBar#onDisplayReady(int)
@@ -895,6 +897,14 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
     }
 
     @Override
+    public void setBlockedGesturalNavigation(boolean blocked) {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_SET_BLOCKED_GESTURAL_NAVIGATION);
+            mHandler.obtainMessage(MSG_SET_BLOCKED_GESTURAL_NAVIGATION, blocked).sendToTarget();
+        }
+    }
+
+    @Override
     public void onDisplayReady(int displayId) {
         synchronized (mLock) {
             mHandler.obtainMessage(MSG_DISPLAY_READY, displayId, 0).sendToTarget();
@@ -1410,6 +1420,11 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
                 case MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).leftInLandscapeChanged(msg.arg1 != 0);
+                    }
+                    break;
+                case MSG_SET_BLOCKED_GESTURAL_NAVIGATION:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).setBlockedGesturalNavigation((Boolean) msg.obj);
                     }
                     break;
             }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -229,6 +229,8 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
     private int mSysUiFlags;
 
     private boolean mEdgeHapticEnabled;
+    private boolean mBlockedGesturalNavigation;
+
     private static final int HAPTIC_DURATION = 20;
 
     private int mEdgeHeight;
@@ -1047,6 +1049,10 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
         return topActivity != null && mGestureBlockingActivities.contains(topActivity);
     }
 
+    public void setBlockedGesturalNavigation(boolean blocked) {
+        mBlockedGesturalNavigation = blocked;
+    }
+
     @Override
     public void writeToProto(SystemUiTraceProto proto) {
         if (proto.edgeBackGestureHandler == null) {
@@ -1061,7 +1067,9 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
         }
 
         public void onInputEvent(InputEvent event) {
-            EdgeBackGestureHandler.this.onInputEvent(event);
+            if (!mBlockedGesturalNavigation) {
+                EdgeBackGestureHandler.this.onInputEvent(event);
+            }
             finishInputEvent(event, true);
         }
     }
