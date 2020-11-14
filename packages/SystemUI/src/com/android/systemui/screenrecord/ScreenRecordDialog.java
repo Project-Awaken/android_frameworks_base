588a4623fa62e5df83171b443e79d854e7217473
ezio84 <brabus84@gmail.com>
2020-11-14 22:23:23 +0100

    Screenrecord: add blinking stop dot and low quality options
    We could want to stop the recording without expanding the notification
    panel and show sensitive info.
    Long pressing on the red dot moves it to the opposite side,
    in case the user wants not to cover specific elements on the screen.
    
    Add also a low quality option that still gives a good recording
    reducing a lot the final file size (useful when sharing the screenrecord
    on data connections).
    
    old commits ref:
    https://github.com/ezio84/abc_frameworks_base/commit/6092d9ca8b0949d64c1b87d6cd53d66daf002e14
    https://github.com/ezio84/abc_frameworks_base/commit/b50c7caf8b46c731093b11f5647028a23346f937
    https://github.com/ezio84/abc_frameworks_base/commit/91c00e8fb70cb17b8658678bf476452f96cd44d1
    
    idoybh (YAAP): Adapted to A12
    
    Change-Id: If413241c3fa533650690cf3b7df5c05fb2f8c8ed
    
    -----
    
    Screenrecord: Make low quality bitrate scalable per device
    
    Hack our way around that weird formula
    Just devide scaled bitrate by 3 for low quality
    
    Change-Id: I7669769ca3104b3566ed38f549af384f83d92d81
    
    -----
    
    Screenrecord: Save and load set preferences
    
    It's very annoying setting em back for each recording
    Save preferences on start, load back when dialog opens
    
    Change-Id: Ic8318cdb72a64da8eeb7d0966d45314d450d2d39
    
    -----
    
    Screenrecord: Add an option to record for longer
    
    Unlimit time-wise. Only limit file size to 15GiB
    Change-Id: I85fe7ab5a0b019a34403f707f24566e627c65342
    
    Change-Id: Ia16045dde478f65d1bee4a5167684c403f018906
    
    -----
    
    SystemUI: Adapt screenrecord dialog switches UI to 13
    
    Change-Id: Idb60f9294b409de218ccc2282542ef8e8e2d379e
    
    -----
    
    Screenrecord: Update custom settings dialog drawables
    
    Change-Id: I3f0c05854954fcf2d79f9b48a862cfd6160e5338
    
    -----
    
    SystemUI: screenrecord: Use sdcard icon for file size limit toggle
    
    Change-Id: I92c6af726b8352b956813a046c72902b2cae6aa2
    
    -----
    
    SystemUI: screenrecord: Add an option to use HEVC
    
    Chose the profiles that best fit the ones we have in H.264 by spec
    
    Change-Id: I0e4428edbe277c9a918e2d1447f48ee8cab32b19
    
    -----
    
    SystemUI: screenrecord: Shorten strings a bit
    
    Change-Id: I4b354ddc9b49c00ab04c5a42033f7af71193a207
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordDialog.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordDialog.java
@@ -44,6 +44,7 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
+import com.android.systemui.Prefs;
 import com.android.systemui.R;
 import com.android.systemui.animation.ActivityLaunchAnimator;
 import com.android.systemui.animation.DialogLaunchAnimator;
@@ -66,6 +67,14 @@ public class ScreenRecordDialog extends SystemUIDialog {
             MIC_AND_INTERNAL);
     private static final long DELAY_MS = 3000;
     private static final long INTERVAL_MS = 1000;
+    private static final String PREFS = "screenrecord_";
+    private static final String PREF_TAPS = "show_taps";
+    private static final String PREF_DOT = "show_dot";
+    private static final String PREF_LOW = "use_low_quality";
+    private static final String PREF_LONGER = "use_longer_timeout";
+    private static final String PREF_HEVC = "use_hevc";
+    private static final String PREF_AUDIO = "use_audio";
+    private static final String PREF_AUDIO_SOURCE = "audio_source";
 
     private final RecordingController mController;
     private final UserContextProvider mUserContextProvider;
@@ -75,6 +84,10 @@ public class ScreenRecordDialog extends SystemUIDialog {
     private final FeatureFlags mFlags;
     private final DialogLaunchAnimator mDialogLaunchAnimator;
     private Switch mTapsSwitch;
+    private Switch mStopDotSwitch;
+    private Switch mLowQualitySwitch;
+    private Switch mLongerSwitch;
+    private Switch mHEVCSwitch;
     private Switch mAudioSwitch;
     private Spinner mOptions;
 
@@ -147,6 +160,10 @@ public class ScreenRecordDialog extends SystemUIDialog {
 
         mAudioSwitch = findViewById(R.id.screenrecord_audio_switch);
         mTapsSwitch = findViewById(R.id.screenrecord_taps_switch);
+        mStopDotSwitch = findViewById(R.id.screenrecord_stopdot_switch);
+        mLowQualitySwitch = findViewById(R.id.screenrecord_lowquality_switch);
+        mLongerSwitch = findViewById(R.id.screenrecord_longer_timeout_switch);
+        mHEVCSwitch = findViewById(R.id.screenrecord_hevc_switch);
         mOptions = findViewById(R.id.screen_recording_options);
         ArrayAdapter a = new ScreenRecordingAdapter(getContext().getApplicationContext(),
                 android.R.layout.simple_spinner_dropdown_item,
@@ -156,6 +173,8 @@ public class ScreenRecordDialog extends SystemUIDialog {
         mOptions.setOnItemClickListenerInt((parent, view, position, id) -> {
             mAudioSwitch.setChecked(true);
         });
+
+        loadPrefs();
     }
 
     /**
@@ -165,7 +184,12 @@ public class ScreenRecordDialog extends SystemUIDialog {
      */
     private void requestScreenCapture(@Nullable MediaProjectionCaptureTarget captureTarget) {
         Context userContext = mUserContextProvider.getUserContext();
+        savePrefs(userContext);
         boolean showTaps = mTapsSwitch.isChecked();
+        boolean showStopDot = mStopDotSwitch.isChecked();
+        boolean lowQuality = mLowQualitySwitch.isChecked();
+        boolean longerDuration = mLongerSwitch.isChecked();
+        boolean hevc = mHEVCSwitch.isChecked();
         ScreenRecordingAudioSource audioMode = mAudioSwitch.isChecked()
                 ? (ScreenRecordingAudioSource) mOptions.getSelectedItem()
                 : NONE;
@@ -173,7 +197,8 @@ public class ScreenRecordDialog extends SystemUIDialog {
                 RecordingService.REQUEST_CODE,
                 RecordingService.getStartIntent(
                         userContext, Activity.RESULT_OK,
-                        audioMode.ordinal(), showTaps, captureTarget),
+                        audioMode.ordinal(), showTaps, captureTarget,
+                        showStopDot, lowQuality, longerDuration, hevc),
                 PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         PendingIntent stopIntent = PendingIntent.getService(userContext,
                 RecordingService.REQUEST_CODE,
@@ -198,5 +223,26 @@ public class ScreenRecordDialog extends SystemUIDialog {
                 requestScreenCapture(captureTarget);
             }
         }
+    }
+
+    private void savePrefs(Context userContext) {
+        Prefs.putInt(userContext, PREFS + PREF_TAPS, mTapsSwitch.isChecked() ? 1 : 0);
+        Prefs.putInt(userContext, PREFS + PREF_DOT, mStopDotSwitch.isChecked() ? 1 : 0);
+        Prefs.putInt(userContext, PREFS + PREF_LOW, mLowQualitySwitch.isChecked() ? 1 : 0);
+        Prefs.putInt(userContext, PREFS + PREF_LONGER, mLongerSwitch.isChecked() ? 1 : 0);
+        Prefs.putInt(userContext, PREFS + PREF_HEVC, mHEVCSwitch.isChecked() ? 1 : 0);
+        Prefs.putInt(userContext, PREFS + PREF_AUDIO, mAudioSwitch.isChecked() ? 1 : 0);
+        Prefs.putInt(userContext, PREFS + PREF_AUDIO_SOURCE, mOptions.getSelectedItemPosition());
+    }
+
+    private void loadPrefs() {
+        Context userContext = mUserContextProvider.getUserContext();
+        mTapsSwitch.setChecked(Prefs.getInt(userContext, PREFS + PREF_TAPS, 0) == 1);
+        mStopDotSwitch.setChecked(Prefs.getInt(userContext, PREFS + PREF_DOT, 0) == 1);
+        mLowQualitySwitch.setChecked(Prefs.getInt(userContext, PREFS + PREF_LOW, 0) == 1);
+        mLongerSwitch.setChecked(Prefs.getInt(userContext, PREFS + PREF_LONGER, 0) == 1);
+        mHEVCSwitch.setChecked(Prefs.getInt(userContext, PREFS + PREF_HEVC, 1) == 1);
+        mAudioSwitch.setChecked(Prefs.getInt(userContext, PREFS + PREF_AUDIO, 0) == 1);
+        mOptions.setSelection(Prefs.getInt(userContext, PREFS + PREF_AUDIO_SOURCE, 0));
     }
 }
