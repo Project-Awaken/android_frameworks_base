33f661d9a36b0c99b2fd2f754a58a8372061d7a1
Jyotiraditya Panda <jyotiraditya@aospa.co>
2020-12-13 18:13:56 +0000

    SystemUI: Stop building NotoSerif font.
    biggest step taken, kills remaining 0.1% of germs completely.
    it's true tho.
    
    Change-Id: I26da2afc4d77749c735492345e5228899e79a2e4
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -47,7 +47,6 @@ LOCAL_REQUIRED_MODULES := \
     DisplayCutoutEmulationHoleOverlay \
 	DisplayCutoutEmulationTallOverlay \
 	DisplayCutoutEmulationWaterfallOverlay \
-	FontNotoSerifSourceOverlay \
 	FontManropeOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularLauncherOverlay \
