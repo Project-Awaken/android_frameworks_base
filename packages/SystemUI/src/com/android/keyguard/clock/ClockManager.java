909917c5c6514134d1fcdb4298d0b1022096a959
ZeNiXxX <zenixxx.havoc@gmail.com>
2021-01-02 18:41:05 +0000

    Clock Style : Add lockscreen Bubbles animation clock
    @ZeNiXxX:
    * Bringup

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -168,6 +168,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new MNMLMinimalClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new SwingClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new LoadingClockController(res, layoutInflater, colorExtractor, context));
+        addBuiltinClock(() -> new BubbleAnimationClockController(res, layoutInflater, colorExtractor, context));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
