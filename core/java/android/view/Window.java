Project-Xtended/frameworks_base

Camera: Rename prevent power key permission
* We must not use the "android" namespace or CTS fails.

core/java/android/view/Window.java
@@ -1199,7 +1199,7 @@ public abstract class Window {
 
     private void setPrivateFlags(int flags, int mask) {
         if ((flags & mask & WindowManager.LayoutParams.PRIVATE_FLAG_PREVENT_POWER_KEY) != 0) {
-            mContext.enforceCallingOrSelfPermission("android.permission.PREVENT_POWER_KEY",
+            mContext.enforceCallingOrSelfPermission("lineage.permission.PREVENT_POWER_KEY",
                     "No permission to prevent power key");
         }
         final WindowManager.LayoutParams attrs = getAttributes();

Project-Awaken/android_frameworks_base

core/java/android/view/Window.java
@@ -1199,7 +1199,7 @@ public abstract class Window {
 
     private void setPrivateFlags(int flags, int mask) {
         if ((flags & mask & WindowManager.LayoutParams.PRIVATE_FLAG_PREVENT_POWER_KEY) != 0) {
-            mContext.enforceCallingOrSelfPermission("android.permission.PREVENT_POWER_KEY",
+            mContext.enforceCallingOrSelfPermission("awaken.permission.PREVENT_POWER_KEY",
                     "No permission to prevent power key");
         }
         final WindowManager.LayoutParams attrs = getAttributes();