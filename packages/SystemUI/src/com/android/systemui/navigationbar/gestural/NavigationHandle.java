908f525e7fdf5eca8503b678beb6109c372751c0
Ido Ben-Hur <idoybh2@gmail.com>
2021-01-15 12:41:40 +0200

    NavigationHandle: Only register callbacks when attached
    And remove when detached
    Also use the handler thread for maybeMakeVisible()
    
    Change-Id: Ia629294cb179ba1e220a4c3be11a0dc5e875ef35

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/NavigationHandle.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/NavigationHandle.java
@@ -70,7 +70,6 @@ public class NavigationHandle extends View implements ButtonInterface {
             };
 
     private KeyguardUpdateMonitor mUpdateMonitor;
-
     private KeyguardUpdateMonitorCallback mMonitorCallback = new KeyguardUpdateMonitorCallback() {
         @Override
         public void onDreamingStateChanged(boolean dreaming) {
@@ -121,7 +120,6 @@ public class NavigationHandle extends View implements ButtonInterface {
         setFocusable(false);
 
         mUpdateMonitor = Dependency.get(KeyguardUpdateMonitor.class);
-        mUpdateMonitor.registerCallback(mMonitorCallback);
     }
 
     @Override
@@ -225,5 +223,17 @@ public class NavigationHandle extends View implements ButtonInterface {
 
     private float getPulseAnimationProgress() {
         return mPulseAnimationProgress;
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        mUpdateMonitor.registerCallback(mMonitorCallback);
+        super.onAttachedToWindow();
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        mUpdateMonitor.removeCallback(mMonitorCallback);
+        super.onDetachedFromWindow();
     }
 }
