579f48d92d2ba7a0ed471bd51a00d3bca60ab46d
zoldycc <sanjheevisv@gmail.com>
2021-03-06 08:00:23 +0530

    base: Smart Cutoff (1/2)
    Co-authored-by: Lucchetto <lucchetto.tie@live.com>
    Co-authored-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: zoldycc <sanjheevisv@gmail.com>
    Change-Id: Id8259ca13c9dc1c9c2b34f375471bd4fafb27bec

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5970,6 +5970,21 @@ public final class Settings {
         public static final String SHOW_APP_VOLUME = "show_app_volume";
 
         /**
+         * @hide
+         */
+        public static final String SMART_CUTOFF = "smart_cutoff";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CUTOFF_TEMPERATURE = "smart_cutoff_temperature";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CUTOFF_RESUME_TEMPERATURE = "smart_cutoff_resume_temperature";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
