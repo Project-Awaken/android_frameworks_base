e8560d8257500c79c825fd6c5dcc1025e96ebbea
Danny Lin <danny@kdrag0n.dev>
2021-10-05 21:01:09 -0700

    SystemUI: Require unlocking to use sensitive QS tiles
    - Airplane mode
    - Battery saver
    - Bluetooth
    - Cellular
    - DataSaver
    - Hotspot
    - NFC
    - Rotation lock
    - Wi-Fi
    - Work mode
    
    This is the same idea as the following commits from GrapheneOS, but
    simplified to reduce changes made to each individual QS tile:
    
    commit 45670099ed63c9fbde6ff1bd13d27b674416d21d
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:22:41 2020 -0700
    
        require unlocking to use work mode tile
    
    commit 4b7c9559dfba84e10142fcd53b3edac9c75b0748
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:21:16 2020 -0700
    
        require unlocking to use battery saver tile
    
    commit f45c24a4728531441c60320eda0a4fce4285de06
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:19:36 2020 -0700
    
        require unlocking to use cellular quick tile
    
    commit c18f6f868aee5b8f27fcc1966089f814f9f89ab2
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:15:46 2020 -0700
    
        require unlocking to use hotspot quick tile
    
    commit 442c94e1ee499eed467863362f8eb62bb88027a4
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:13:58 2020 -0700
    
        require unlocking to use data saver quick tile
    
    commit cf84259708f9bf650c8494e16025725b1bfa5e23
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:11:58 2020 -0700
    
        require unlocking to use rotation lock quick tile
    
    commit 61eb5a6392c53e4d44cb91cdb4df27c3055ef439
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:08:33 2020 -0700
    
        require unlocking to use Wi-Fi quick tile
    
    commit ad122f0a5a46e2445b5866d3ff83fdc7623b76f6
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:05:18 2020 -0700
    
        require unlocking to use airplane mode quick tile
    
    commit a3686b6f905fb734b3a4f6db545e8a3ef3fc3cc7
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:02:20 2020 -0700
    
        require unlocking to use Bluetooth quick tile
    
    commit 3046535a40f53835eb0b57d4c12a29828ba90f30
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 16:47:59 2020 -0700
    
        require unlocking to use NFC quick tile
    
    Change-Id: I515f4bdd30f84a4a76f4807afcf3cc5a3ce42fa9
    
    -----
    
    SystemUI: InternetTile: We should be secure as well
    
    -----
    
    SystemUI: LocationTile: Be affected by the secure tiles toggle
    
    This was already secure in AOSP, let's make our toggle affect it as well
    
    Change-Id: I3428cd90163350e0d01657e0dff950f76ba56328
    
    -----
    
    SystemUI: Require unlocking to use USB Tether QS tile
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I765341e824a0e1a16c843bbf9edb3fa7a8d3daf2
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Co-authored-by: inthewaves <inthewaves@pm.me>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/UsbTetherTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/UsbTetherTile.java
@@ -41,6 +41,8 @@ import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.FalsingManager;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.statusbar.policy.KeyguardStateController;
+import com.android.systemui.qs.pipeline.domain.interactor.PanelInteractor;
 import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.QsEventLogger;
 import com.android.systemui.qs.logging.QSLogger;
@@ -62,6 +64,10 @@ public class UsbTetherTile extends QSTileImpl<BooleanState> {
 
     private final TetheringManager mTetheringManager;
 
+    private final ActivityStarter mActivityStarter;
+    private final KeyguardStateController mKeyguard;
+    private final PanelInteractor mPanelInteractor;
+
     private boolean mListening;
 
     private boolean mUsbConnected = false;
@@ -77,11 +83,23 @@ public class UsbTetherTile extends QSTileImpl<BooleanState> {
             MetricsLogger metricsLogger,
             StatusBarStateController statusBarStateController,
             ActivityStarter activityStarter,
-            QSLogger qsLogger
+            QSLogger qsLogger,
+            KeyguardStateController keyguardStateController,
+            PanelInteractor panelInteractor
     ) {
         super(host, uiEventLogger, backgroundLooper, mainHandler, falsingManager, metricsLogger,
                 statusBarStateController, activityStarter, qsLogger);
         mTetheringManager = mContext.getSystemService(TetheringManager.class);
+        mPanelInteractor = panelInteractor;
+        mActivityStarter = activityStarter;
+        mKeyguard = keyguardStateController;
+        final KeyguardStateController.Callback callback = new KeyguardStateController.Callback() {
+            @Override
+            public void onKeyguardShowingChanged() {
+                refreshState();
+            }
+        };
+        mKeyguard.observe(this, callback);
     }
 
     public BooleanState newTileState() {
@@ -103,11 +121,22 @@ public class UsbTetherTile extends QSTileImpl<BooleanState> {
         }
     }
 
-    @Override
-    protected void handleClick(@Nullable View view) {
+    private void handleClickInner(@Nullable View view) {
         if (mUsbConnected) {
             mTetheringManager.setUsbTethering(!mUsbTetherEnabled);
         }
+    }
+
+    @Override
+    protected void handleClick(@Nullable View view) {
+        if (mKeyguard.isMethodSecure() && mKeyguard.isShowing()) {
+            mActivityStarter.postQSRunnableDismissingKeyguard(() -> {
+                mPanelInteractor.openPanels();
+                handleClickInner(view);
+            });
+            return;
+        }
+        handleClickInner(view);
     }
 
     @Override
