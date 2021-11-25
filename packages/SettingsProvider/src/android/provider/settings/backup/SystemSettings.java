eaa024e4d7c4f2eb267ee498bcf68dd211e08ffa
jhonboy121 <alfredmathew05@gmail.com>
2021-11-25 17:32:41 +0530

    base: Allow hiding navbar [1/3]
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: I7fb60efbe3792695a33c678273b88ac518bd83a6

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -94,5 +94,6 @@ public class SystemSettings {
         Settings.System.KEYGAURD_MEDIA_ART,
         Settings.System.AUTO_BRIGHTNESS_ONE_SHOT,
         Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL,
+        Settings.System.FULLSCREEN_GESTURES,
     };
 }
