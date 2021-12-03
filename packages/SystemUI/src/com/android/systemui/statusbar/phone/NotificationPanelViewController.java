267920fcb950ae4a0155f30425a4e82edcc46381
ezio84 <brabus84@gmail.com>
2021-12-03 19:51:29 +0530

    base: Add API for disabling gestural navigation
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Wed Mar 23 18:40:00 2022 +0530
    
        base: Update API for disabling gestural navigation
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I0174ff0e8a30f0ef2b54c1f61145a575b844ad72

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -654,6 +654,8 @@ public class NotificationPanelViewController extends PanelViewController {
 
     private final TunerService mTunerService;
 
+    private boolean mBlockedGesturalNavigation;
+
     private View.AccessibilityDelegate mAccessibilityDelegate = new View.AccessibilityDelegate() {
         @Override
         public void onInitializeAccessibilityNodeInfo(View host, AccessibilityNodeInfo info) {
@@ -4254,6 +4256,10 @@ public class NotificationPanelViewController extends PanelViewController {
         mContentResolver.unregisterContentObserver(mSettingsChangeObserver);
     }
 
+    public void setBlockedGesturalNavigation(boolean blocked) {
+        mBlockedGesturalNavigation = blocked;
+    }
+
     /**
      * Updates notification panel-specific flags on {@link SysUiState}.
      */
@@ -4264,7 +4270,8 @@ public class NotificationPanelViewController extends PanelViewController {
         }
         mSysUiState.setFlag(SYSUI_STATE_NOTIFICATION_PANEL_EXPANDED,
                 isFullyExpanded() && !isInSettings())
-                .setFlag(SYSUI_STATE_QUICK_SETTINGS_EXPANDED, isInSettings())
+                .setFlag(SYSUI_STATE_QUICK_SETTINGS_EXPANDED,
+                            mBlockedGesturalNavigation || isInSettings())
                 .commitUpdate(mDisplayId);
     }
 
