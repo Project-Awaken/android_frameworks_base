6c11f14c974ab034e115b161c453b904db19d5f7
Ido Ben-Hur <idoybh2@gmail.com>
2021-12-23 17:24:28 +0200

    base: Add a toggle for secure tiles on keyguard [1/2]
    Change-Id: I5a5fcd78ffb7abd35324f3ad598a22b30f3a929a
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12088,6 +12088,13 @@ public final class Settings {
         public static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
 
         /**
+         * Whether to allow sensitive qs tiles on keyguard
+         * @hide
+         */
+        @Readable
+        public static final String QSTILE_REQUIRES_UNLOCKING = "qstile_requires_unlocking";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
