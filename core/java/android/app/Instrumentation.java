9089c82090384d3be67b83d5b9f6cdcf977a07f3
Danny Lin <danny@kdrag0n.dev>
2022-01-06 20:51:18 +0200

    core: Make build property spoofing more reliable
    Determining the package name from Zygote's post-fork process name is
    unreliable for several reasons:
      - If USAP is enabled, Zygote will fork with the USAP process name, and
        spoofing will never trigger when the forked process specializes into
        a specific app.
      - With the native fork loop backported from AOSP master,
        Zygote.setAppProcessName() is no longer used for most apps because
        the forking is handled by native code.
    
    Instrumentation#newApplication() is responsible for creating the
    Application singleton during each app's initialization, so hook it
    instead for a more reliable and robust trigger.
    
    Change-Id: I94c7ffd5d832637704ca196b4af7b9937e7204d9

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -57,6 +57,8 @@ import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
 
+import com.android.internal.util.custom.PixelPropsUtils;
+
 import java.io.File;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -1192,6 +1194,7 @@ public class Instrumentation {
         app.attach(context);
         String packageName = app.getPackageName();
         PixelPropsUtils.setProps(packageName);
+        PixelPropsUtils.setProps(app.getPackageName());
         return app;
     }
     
@@ -1211,6 +1214,7 @@ public class Instrumentation {
         app.attach(context);
         String packageName = app.getPackageName();
         PixelPropsUtils.setProps(packageName);
+        PixelPropsUtils.setProps(app.getPackageName());
         return app;
     }
 
