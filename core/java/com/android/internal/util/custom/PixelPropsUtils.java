1ecb6433e66b440b7a5bc521117571972f8584b4
Joey Huab <joey@evolution-x.org>
2022-01-27 13:33:48 +0900

    PixelPropsUtils: Whitelist ARCore
    * ARCore crashes due to being spoofed as hardware and software mismatch is detected.
    * Crash occurs while being used in Google app search feature.
    
    Change-Id: I008da8b09b6467dca2ceb26649c3cb3dee44777d

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -72,7 +72,8 @@ public class PixelPropsUtils {
         "com.google.android.GoogleCameraEng2",
         "com.google.android.MTCL83",
         "com.google.android.UltraCVM",
-        "com.google.android.apps.cameralite"
+        "com.google.android.apps.cameralite",
+        "com.google.ar.core"
     };
 
     private static ArrayList<String> allProps = new ArrayList<>(Arrays.asList("BRAND", "MANUFACTURER", "DEVICE", "PRODUCT", "MODEL", "FINGERPRINT"));
