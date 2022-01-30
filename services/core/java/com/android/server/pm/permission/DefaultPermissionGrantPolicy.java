447991b5f51d00f50945ac7d4010de88b489e3d2
Pranav Vashi <neobuddy89@gmail.com>
2022-01-30 15:37:30 +0530

    DefaultPermissionGrant: Fix google search crash
    * Google search is not capable asking runtime permission for Phone State.
      This leads to crash unless user provides this permission explicity from
      app info.
    * Grant phone state permission by default. Rest permissions are fine to be
      provided on runtime.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I7cb61712dcdbd0ea04c3b8489cb9609243e5aca1

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -1010,6 +1010,11 @@ final class DefaultPermissionGrantPolicy {
 
         // DownloadProviders
         grantSystemFixedPermissionsToSystemPackage(pm,"com.android.providers.downloads.DownloadProvider", userId, STORAGE_PERMISSIONS);
+
+        // Google App
+        grantPermissionsToPackage(pm, "com.google.android.googlequicksearchbox", userId,
+                false /* ignoreSystemPackage */, true /*whitelistRestrictedPermissions*/,
+                PHONE_PERMISSIONS);
     }
 
     private String getDefaultSystemHandlerActivityPackageForCategory(PackageManagerWrapper pm,
