5c3eb17a11055f393e411524b94fc52b02511c32
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-24 02:38:49 +0300

    SystemUI: screenshot: open long screenshot activity for partial screenshots
    The partial screenshot UI is basically unusable. Replace it
    with the long  screenshot UI which offers cropping, saving,
    canceling, and sharing capabilities.
    
    Change-Id: I27f7a811838f7e3f2057f567c7a350255996a8cb
    
    -----
    
    SystemUI: screenshot: remove misplaced call to set crop view padding
    
    This call to setExtraPadding() discards the previously set
    padding, making the screenshot crop view be able to select
    over the borders of the image. Remove it.
    
    Change-Id: I6cc28c79c2d9f4958215bb5957be6c9b2eab1002
    
    -----
    
    SystemUI: screenshot: remove duplicate clamping on move action
    
    On MotionEvent.ACTION_DOWN, a range of of motion is calculated and
    stored. On MotionEvent.ACTION_MOVE, the new position of the crop
    boundary is clamped by the original range of motion, and also
    clamped again by a new range of motion inside setBoundaryPosition().
    
    The original range of motion is pointless and only used in this case,
    remove it.
    
    Change-Id: Ib4f35bf5317f5b5879606680beee3e077f829d10
    
    -----
    
    SystemUI: screenshot: add extra crop boundaries
    
    Add corner boundaries that allow resizing in both vertical and
    horizontal directions at the same time.
    
    To handle corner boundaries, split every boundary into vertical
    and horizontal boundaries, and handle both vertical and horizontal
    changes for all boundaries. If the boundary is a purely vertical
    (top, bottom) or purely horizontal (left, right) boundary, the
    opposite direction boundary will be none, and the selection won't
    be changed on that selection.
    
    Also, add a middle boundary that allows moving the current selection.
    
    To achieve this, treat the middle boundary as composed of top and
    left boundaries, and when trying to change either the top or the
    left boundary while the currently dragged boundary is the middle
    one, reposition the opposite boundary too.
    
    Change-Id: I6dd703733b3d161f5979d91a124350fb1a8d52ae
    
    -----
    
    SystemUI: screenshot: disable magnification for partial screenshots
    
    Partial screenshots are screen-size and don't need a magnifying glass.
    
    Change-Id: I472103352411d1482423230a4ff08ddda45cc53d
    
    -----
    
    SystemUI: screenshot: close QS after launching long screenshot activity
    
    Change-Id: Ief5510c037b27be714f8f6cc8661d81931f3babd
    Co-authored-by: LuK1337 <priv.luk@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -85,6 +85,7 @@ import androidx.concurrent.futures.CallbackToFutureAdapter;
 import com.android.internal.app.ChooserActivity;
 import com.android.internal.logging.UiEventLogger;
 import com.android.internal.policy.PhoneWindow;
+import com.android.internal.statusbar.IStatusBarService;
 import com.android.settingslib.applications.InterestingConfigChanges;
 import com.android.systemui.R;
 import com.android.systemui.broadcast.BroadcastSender;
@@ -259,6 +260,7 @@ public class ScreenshotController {
     private final PhoneWindow mWindow;
     private final DisplayManager mDisplayManager;
     private final ScrollCaptureController mScrollCaptureController;
+    private final IStatusBarService mStatusBarService;
     private final LongScreenshotData mLongScreenshotHolder;
     private final boolean mIsLowRamDevice;
     private final ScreenshotNotificationSmartActionsProvider
@@ -296,6 +298,7 @@ public class ScreenshotController {
             ImageCapture imageCapture,
             @Main Executor mainExecutor,
             ScrollCaptureController scrollCaptureController,
+            IStatusBarService statusBarService,
             LongScreenshotData longScreenshotHolder,
             ActivityManager activityManager,
             TimeoutHandler timeoutHandler,
@@ -310,6 +313,7 @@ public class ScreenshotController {
         mImageCapture = imageCapture;
         mMainExecutor = mainExecutor;
         mScrollCaptureController = scrollCaptureController;
+        mStatusBarService = statusBarService;
         mLongScreenshotHolder = longScreenshotHolder;
         mIsLowRamDevice = activityManager.isLowRamDevice();
         mScreenshotNotificationSmartActionsProvider = screenshotNotificationSmartActionsProvider;
@@ -405,15 +409,8 @@ public class ScreenshotController {
     void takeScreenshotPartial(ComponentName topComponent,
             final Consumer<Uri> finisher, RequestCallback requestCallback) {
         Assert.isMainThread();
-        mScreenshotView.reset();
-        mCurrentRequestCallback = requestCallback;
-
-        attachWindow();
-        mWindow.setContentView(mScreenshotView);
-        mScreenshotView.requestApplyInsets();
-
-        mScreenshotView.takePartialScreenshot(
-                rect -> takeScreenshotInternal(topComponent, finisher, rect));
+        startPartialScreenshotActivity();
+        finisher.accept(null);
     }
 
     /**
@@ -549,6 +546,13 @@ public class ScreenshotController {
                 ClipboardOverlayController.SELF_PERMISSION);
     }
 
+    private Bitmap captureScreenshot() {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getDefaultDisplay().getRealMetrics(displayMetrics);
+        return mImageCapture.captureDisplay(DEFAULT_DISPLAY,
+                new Rect(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels));
+    }
+
     private void saveScreenshot(Bitmap screenshot, Consumer<Uri> finisher, Rect screenRect,
             Insets screenInsets, ComponentName topComponent, boolean showFlash) {
         withWindowAttached(() ->
@@ -671,6 +675,47 @@ public class ScreenshotController {
                 onScrollCaptureResponseReady(future), mMainExecutor);
     }
 
+    public void startLongScreenshotActivity(ScrollCaptureController.LongScreenshot longScreenshot) {
+        mLongScreenshotHolder.setLongScreenshot(longScreenshot);
+        mLongScreenshotHolder.setTransitionDestinationCallback(
+                (transitionDestination, onTransitionEnd) ->
+                        mScreenshotView.startLongScreenshotTransition(
+                                transitionDestination, onTransitionEnd,
+                                longScreenshot));
+        mLongScreenshotHolder.setForegroundAppName(getForegroundAppLabel());
+
+        final Intent intent = new Intent(mContext, LongScreenshotActivity.class);
+        intent.setFlags(
+                Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        mContext.startActivity(intent,
+                ActivityOptions.makeCustomAnimation(mContext, 0, 0).toBundle());
+        RemoteAnimationAdapter runner = new RemoteAnimationAdapter(
+                SCREENSHOT_REMOTE_RUNNER, 0, 0);
+        try {
+            WindowManagerGlobal.getWindowManagerService()
+                    .overridePendingAppTransitionRemote(runner, DEFAULT_DISPLAY);
+        } catch (Exception e) {
+            Log.e(TAG, "Error overriding screenshot app transition", e);
+        }
+
+        try {
+            mStatusBarService.collapsePanels();
+        } catch (RemoteException e) {
+            Log.e(TAG, "Error during collapsing panels", e);
+        }
+    }
+
+    private void startPartialScreenshotActivity() {
+        Bitmap newScreenshot = captureScreenshot();
+
+        ScrollCaptureController.BitmapScreenshot bitmapScreenshot =
+            new ScrollCaptureController.BitmapScreenshot(mContext, newScreenshot);
+
+        mLongScreenshotHolder.setNeedsMagnification(false);
+        startLongScreenshotActivity(bitmapScreenshot);
+    }
+
     private void onScrollCaptureResponseReady(Future<ScrollCaptureResponse> responseFuture) {
         try {
             if (mLastScrollCaptureResponse != null) {
@@ -736,27 +781,8 @@ public class ScreenshotController {
                 return;
             }
 
-            mLongScreenshotHolder.setLongScreenshot(longScreenshot);
-            mLongScreenshotHolder.setTransitionDestinationCallback(
-                    (transitionDestination, onTransitionEnd) ->
-                            mScreenshotView.startLongScreenshotTransition(
-                                    transitionDestination, onTransitionEnd,
-                                    longScreenshot));
-
-            final Intent intent = new Intent(mContext, LongScreenshotActivity.class);
-            intent.setFlags(
-                    Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-            mContext.startActivity(intent,
-                    ActivityOptions.makeCustomAnimation(mContext, 0, 0).toBundle());
-            RemoteAnimationAdapter runner = new RemoteAnimationAdapter(
-                    SCREENSHOT_REMOTE_RUNNER, 0, 0);
-            try {
-                WindowManagerGlobal.getWindowManagerService()
-                        .overridePendingAppTransitionRemote(runner, DEFAULT_DISPLAY);
-            } catch (Exception e) {
-                Log.e(TAG, "Error overriding screenshot app transition", e);
-            }
+            mLongScreenshotHolder.setNeedsMagnification(true);
+            startLongScreenshotActivity(longScreenshot);
         }, mMainExecutor);
     }
 
