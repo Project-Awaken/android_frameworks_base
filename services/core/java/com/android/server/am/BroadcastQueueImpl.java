a4e66e350813750d8b45a834305672e7d04698fe
LibXZR <i@xzr.moe>
2022-08-26 21:54:35 +0800

    am: Block MEDIA_MOUNTED broadcast for restricted apps
    This blocks ACTION_MEDIA_MOUNTED for victims of "strict standby" and
    "force background freezer". Some apps e.g. MobileQQ are using this for
    boot completed notification, which could block the process of serialized
    broadcast for quite a while.
    
    Also move `=` to `.equals()` because the latter one is the proper way
    for comparing strings, although the former one is just working fine
    statically within system server context.
    
    Change-Id: I46a85e7d9f386b10e6001697cf98f77ff058c3e7
    Signed-off-by: LibXZR <i@xzr.moe>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/BroadcastQueueImpl.java
+++ services/core/java/com/android/server/am/BroadcastQueueImpl.java
@@ -991,8 +991,9 @@ public class BroadcastQueueImpl extends BroadcastQueue {
     }
 
 	private boolean isBootCompletedIntent(Intent intent) {
-        return intent.getAction() == Intent.ACTION_BOOT_COMPLETED ||
-                intent.getAction() == Intent.ACTION_LOCKED_BOOT_COMPLETED;
+        return Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction()) ||
+                Intent.ACTION_LOCKED_BOOT_COMPLETED.equals(intent.getAction()) ||
+                Intent.ACTION_MEDIA_MOUNTED.equals(intent.getAction());
     }
 
     public void processNextBroadcastLocked(boolean fromMsg, boolean skipOomAdj) {
@@ -1396,7 +1397,7 @@ public class BroadcastQueueImpl extends BroadcastQueue {
         if (isBootCompletedIntent(r.intent) &&
                 mService.shouldSkipBootCompletedBroadcastForPackage(
                         info.activityInfo.applicationInfo)) {
-            Slog.i(TAG, "BOOT_COMPLETED broadcast skipped because of strict standby for "
+            Slog.i(TAG, "Boot broadcast skipped because of strict standby for "
                     + info.activityInfo.applicationInfo.packageName);
             skip = true;
         }
