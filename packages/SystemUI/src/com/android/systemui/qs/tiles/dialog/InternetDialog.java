ea5b6037adcba99a972ef66702d7f69c30d1325a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-09-18 02:05:07 +0530

    InternetDialog: Launch mobile network settings on long press
    Change-Id: Ibd918c62db65fe42e4028e6cc2c029987f013b59

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialog.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialog.java
@@ -201,7 +201,6 @@ public class InternetDialog extends SystemUIDialog implements
         mCanConfigWifi = canConfigWifi;
         mCanChangeWifiState = WifiEnterpriseRestrictionUtils.isChangeWifiStateAllowed(context);
         mKeyguard = keyguardStateController;
-
         mUiEventLogger = uiEventLogger;
         mDialogLaunchAnimator = dialogLaunchAnimator;
         mAdapter = new InternetAdapter(mInternetDialogController);
@@ -306,6 +305,7 @@ public class InternetDialog extends SystemUIDialog implements
         mHandler.removeCallbacks(mHideProgressBarRunnable);
         mHandler.removeCallbacks(mHideSearchingRunnable);
         mMobileNetworkLayout.setOnClickListener(null);
+        mMobileNetworkLayout.setOnLongClickListener(null);
         mMobileDataToggle.setOnCheckedChangeListener(null);
         mHotspotLayout.setOnClickListener(null);
         mHotspotToggle.setOnCheckedChangeListener(null);
@@ -387,6 +387,10 @@ public class InternetDialog extends SystemUIDialog implements
                     mInternetDialogController.connectCarrierNetwork();
                 }
             }
+        });
+        mMobileNetworkLayout.setOnLongClickListener(v -> {
+                mInternetDialogController.launchMobileNetworkSetting(v);
+                return true;
         });
         mMobileDataToggle.setOnCheckedChangeListener(
                 (buttonView, isChecked) -> {
