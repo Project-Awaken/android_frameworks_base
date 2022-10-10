e646d4687638f9afc136ce7f267b1cafc75dc418
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-10-10 23:06:57 +0530

    SystemUI: Allow disabling screenshot shutter sound [2/2]
    Change-Id: If653666f4bf6ab6600943c0b27d6dc4dab6461ca

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5629,6 +5629,12 @@ public final class Settings {
         public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
 
         /**
+         * Whether to play the camera shutter sound on taking a screenshot.
+         * @hide
+         */
+        public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -65,6 +65,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.RemoteException;
 import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.provider.Settings;
@@ -927,6 +928,11 @@ public class ScreenshotController {
     }
 
     private void playCameraSound() {
+       if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.SCREENSHOT_SHUTTER_SOUND, 1, UserHandle.USER_CURRENT) == 0) {
+           return;
+       }
+
        boolean playSound = readCameraSoundForced() && mCamsInUse > 0;
 
         switch (mAudioManager.getRingerMode()) {
