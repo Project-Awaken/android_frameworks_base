cd4ce22ec3a92638c990a435dabfd8ac2b97e226
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-11-10 01:38:14 +0530

    SystemUI: Allow hiding QS footer data usage [1/2]
    Change-Id: I7ac3093c12728ac6ed63225944d8a10da2ea69ed

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11706,6 +11706,12 @@ public final class Settings {
         public static final String SECURE_LOCKSCREEN_QS_DISABLED = "secure_lockscreen_qs_disabled";
 
         /**
+         * Whether to show daily data usage in the QS footer.
+         * @hide
+         */
+        public static final String QS_SHOW_DATA_USAGE = "qs_show_data_usage";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
