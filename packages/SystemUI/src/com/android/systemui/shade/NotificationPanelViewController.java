60aad2edd67068ebae670ecaeaf6b47df86bea33
Ido Ben-Hur <idoybh2@gmail.com>
2022-12-12 02:24:55 +0200

    NotificationPanelViewController: Delay d2ts on lockscreen by 100ms
    Doing so makes it way more reliable because it doesn't conflict with the keyguard bouncer sleep prevention
    r16 merge made it way worse. this solves it.
    
    Change-Id: Ia56060a6cdb8d01f1d88e7e63b2f1f4eca38f857

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -926,7 +926,9 @@ public final class NotificationPanelViewController implements Dumpable {
                 new GestureDetector.SimpleOnGestureListener() {
             @Override
             public boolean onDoubleTap(MotionEvent e) {
-                AwakenUtils.switchScreenOff(mView.getContext());
+                mView.getHandler().postDelayed(() -> {
+                    AwakenUtils.switchScreenOff(mView.getContext());
+                }, 100);
                 return true;
             }
         });
