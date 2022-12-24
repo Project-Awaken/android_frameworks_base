2ffe17d8abb42304681a1a0bf11f529f54760362
Gokul GK <gk@gkannan.dev>
2022-12-24 19:34:41 +0530

    PixelPropsUtils: Spoof Pixel 7 Pro to SMS Organizer
    This also enables SMS Organizer's copy otp feature without allowing draw over
    other apps
    
    Change-Id: If3450c8e712673de28a2bbc03562f66bcd579b5d

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -68,6 +68,7 @@ public class PixelPropsUtils {
             "com.android.chrome",
             "com.android.vending",
             "com.breel.wallpapers20",
+            "com.microsoft.android.smsorganizer",
             "com.nothing.smartcenter"
     };
 
