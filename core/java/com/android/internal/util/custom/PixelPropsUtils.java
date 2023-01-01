0aabe1525fd2bc8a0c0abe40512fe33a3ed7a7bc
jhenrique09 <jhsv09@gmail.com>
2023-01-01 13:37:37 -0300

    PixelPropsUtils: Use redfin props for extra packages
    Change-Id: If51bba2e21c722301e57a85bad8387479fb83574
    (cherry picked from commit 8a6da0b93664cfea9e30000bb38bf987f321be89)

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -129,9 +129,7 @@ public class PixelPropsUtils {
                 sIsFinsky = true;
                 return;
             } else {
-                if ((Arrays.asList(packagesToChangePixel7Pro).contains(packageName))
-                        || packageName.startsWith(SAMSUNG)
-                        || Arrays.asList(extraPackagesToChange).contains(packageName)) {
+                if ((Arrays.asList(packagesToChangePixel7Pro).contains(packageName))) {
                     propsToChange.putAll(propsToChangePixel7Pro);
                 } else if (Arrays.asList(packagesToChangePixelXL).contains(packageName)) {
                     propsToChange.putAll(propsToChangePixelXL);
