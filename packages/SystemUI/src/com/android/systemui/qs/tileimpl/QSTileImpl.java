02d18352789d0db62c38d702d2cd901e25963f53
Krishnakant Shedge <shedgekrishnakant33@gmail.com>
2023-01-03 17:57:43 +0000

    SystemUI: Use click effect instead of duration for haptic feedback for QSTile
    Change-Id: I532190cf4c082faaecce57dc7c4b4423f679ef2c
    Signed-off-by: Krishnakant Shedge <shedgekrishnakant33@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -40,6 +40,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.UserHandle;
 import android.os.Vibrator;
+import android.os.VibrationEffect;
 import android.provider.Settings;
 import android.text.format.DateUtils;
 import android.util.ArraySet;
@@ -208,7 +209,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
 
         resetStates();
         mUiHandler.post(() -> mLifecycle.setCurrentState(CREATED));
-        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+        mVibrator = mContext.getSystemService(Vibrator.class);
     }
 
     protected final void resetStates() {
@@ -282,10 +283,10 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
                 Settings.Secure.QUICK_SETTINGS_TILES_VIBRATE, 0, UserHandle.USER_CURRENT) == 1);
     }
 
-    public void vibrateTile(int duration) {
+    public void vibrateTile() {
         if (!isVibrationEnabled()) { return; }
         if (mVibrator != null) {
-            if (mVibrator.hasVibrator()) { mVibrator.vibrate(duration); }
+            if (mVibrator.hasVibrator()) { mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_CLICK)); }
         }
     }
 
@@ -313,7 +314,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
         if (!mFalsingManager.isFalseTap(FalsingManager.LOW_PENALTY)) {
             mHandler.obtainMessage(H.CLICK, eventId, 0, view).sendToTarget();
         }
-        vibrateTile(45);
+        vibrateTile();
     }
 
     public void secondaryClick(@Nullable View view) {
@@ -341,7 +342,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
         if (!mFalsingManager.isFalseLongTap(FalsingManager.LOW_PENALTY)) {
             mHandler.obtainMessage(H.LONG_CLICK, eventId, 0, view).sendToTarget();
         }
-        vibrateTile(45);
+        vibrateTile();
     }
 
     public LogMaker populate(LogMaker logMaker) {
