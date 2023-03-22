a66b330b56337c315d97834903e3d893b04cb3f8
John Galt <johngaltfirstrun@gmail.com>
2023-03-22 11:32:52 -0400

    PixelPropsUtils: move snapchat to walleye spoofing
    Snapchat on A13 without spoofing has delayed messages issue on many
    devices. Spoofing to newer pixels does not resolve this and additionally
    can harm photo quality on non pixels. However spoofing to an older
    device and android version such as Walleye resolve the delayed messages
    without harming photo quality.
    
    Change-Id: I25e77fbe0e93bccd2437771562624a620f88dac4
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -61,7 +61,8 @@ public class PixelPropsUtils {
     };
 
     private static final String[] packagesToChangePixelXL = {
-            "com.google.android.apps.photos"
+            "com.google.android.apps.photos",
+            "com.snapchat.android"
     };
 
     private static final String[] extraPackagesToChange = {
