f22b01b9ee17d95be7ae9d6eda26ab7ae4eb5b46
PainKiller3 <ninadpatil100@gmail.com>
2024-02-06 15:11:56 +0530

    PIHooks: Update fingerprint To February 2024 Security Patch
    Change-Id: Ic37c1c65fdc50eb0ab92bb683682bef2db537aca

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -83,7 +83,7 @@ public class PropImitationHooks {
         "MANUFACTURER", "Google",
         "BRAND", "google",
         "MODEL", "Pixel 8 Pro",
-        "FINGERPRINT", "google/husky/husky:14/UQ1A.240105.004/11206848:user/release-keys"
+        "FINGERPRINT", "google/husky/husky:14/UQ1A.240205.004/11269751:user/release-keys"
     );
 
     private static final Map<String, String> sPixelOneProps = Map.of(
